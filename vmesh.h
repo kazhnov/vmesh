556e5191c06765cd309d6cbca07382120ebaa118
Vanya Kazhnoff <iwan.kasinowe@gmail.com>
2026-02-03 18:13:15 +0700

    minor fix

--- vmesh.h
+++ vmesh.h
@@ -11,9 +11,9 @@ uint32_t VMESH_VertexCount(Mesh* mesh);
 
 uint32_t* VMESH_Faces(Mesh* mesh);
 
-uint32_t VMESH_IndicesSize(Mesh* mesh);
+uint32_t VMESH_IndicesCount(Mesh* mesh);
 
-uint32_t VMESH_FacesCound(Mesh* mesh);
+uint32_t VMESH_FacesCount(Mesh* mesh);
 
 
 Mesh *VMESH_LoadObj(char* path);
