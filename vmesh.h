85308510caf24a672c469e2c498d66ecde5304ff
Vanya Kazhnoff <iwan.kasinowe@gmail.com>
2026-02-03 18:07:49 +0700

    minor fix

--- vmesh.h
+++ vmesh.h
@@ -2,10 +2,18 @@
 struct Mesh;
 typedef struct Mesh Mesh;
 
-float* VMESH_Vertices(Mesh* mesh);
-uint32_t VMESH_VerticesSize(Mesh* mesh);
 
-uint32_t* VMESH_Indices(Mesh* mesh);
+float* VMESH_Vertices(Mesh* mesh);
+
+uint32_t VMESH_FloatsCount(Mesh* mesh);
+
+uint32_t VMESH_VertexCount(Mesh* mesh);
+
+uint32_t* VMESH_Faces(Mesh* mesh);
+
 uint32_t VMESH_IndicesSize(Mesh* mesh);
+
+uint32_t VMESH_FacesCound(Mesh* mesh);
+
 
 Mesh *VMESH_LoadObj(char* path);
