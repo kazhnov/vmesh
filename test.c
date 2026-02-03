33c04b61a124d687fce610291ffa0b79ae12151e
Vanya Kazhnoff <iwan.kasinowe@gmail.com>
2026-02-04 01:14:08 +0700

    added normals

--- test.c
+++ test.c
@@ -1,9 +1,8 @@
 #include "vmesh.h"
 #include <stdio.h>
 
-int main() {
-    
-    Mesh* mesh =VMESH_LoadObj("teapot.obj");
+int main() {    
+    Mesh* mesh =VMESH_LoadObj("teapot_normals.obj");
     printf("vertices: %d, faces: %d\n", VMESH_VertexCount(mesh), VMESH_IndicesCount(mesh));
     printf("OK\n");
     return 0;
--- vmesh.c
+++ vmesh.c
@@ -36,16 +36,21 @@ uint32_t VMESH_FacesCount(Mesh* mesh) {
     return mesh->index_count/3;
 }
 
-void iVMESH_CountVerticesAndFaces(char* path, uint32_t* vertices, uint32_t *faces) {
+void iVMESH_CountVerticesAndFaces(char* path, uint32_t* vertices, uint32_t *faces, uint32_t *normals) {
     FILE* file = fopen(path, "r");
     char c;
     *faces = 0;
     *vertices = 0;
+    *normals = 0;
     
     while ((c = fgetc(file)) != EOF) {
 	if (c == '#');
 	if (c == 'f') *faces += 1;
-	if (c == 'v') *vertices += 1;
+	if (c == 'v') {
+	    c = fgetc(file);
+	    if (c == 'n') *normals += 1;
+	    else *vertices += 1;
+	}
 	while (c!= '\n' && c != EOF) {
 	    c = fgetc(file);
 	}
@@ -59,47 +64,54 @@ Mesh *VMESH_LoadObj(char* path) {
     mesh->vertex_size = 3+4;
     FILE* file = fopen(path, "r");
     char c;
-    float *vertex;
     uint32_t *index;
-    uint32_t vertex_count, face_count;
-    iVMESH_CountVerticesAndFaces(path, &vertex_count, &face_count);
-    printf("vertices: %d, faces: %d\n", vertex_count, face_count);
+    uint32_t vertex_count, face_count, normal_count;
+    iVMESH_CountVerticesAndFaces(path, &vertex_count, &face_count, &normal_count);
+    printf("vertices: %d, faces: %d, normals: %d\n", vertex_count, face_count, normal_count);
+
+    if (vertex_count != 0 && vertex_count != normal_count) {
+	printf("vertex count and normal count are not the same!\n");
+	return NULL;
+    }
     
     uint32_t floats_count = mesh->vertex_size * vertex_count;
-    mesh->vertices = malloc(sizeof(float)*floats_count);
+    mesh->vertices = calloc(sizeof(float),floats_count);
     mesh->floats_count = floats_count;
-
     
     uint32_t index_count = face_count*3;
-    mesh->faces = malloc(sizeof(uint32_t)*index_count);
+    mesh->faces = calloc(sizeof(uint32_t), index_count);
     mesh->index_count = index_count;
 
-    vertex = mesh->vertices;    
+    
+    uint32_t vertex = 0;
+    uint32_t normal = 0;
     index = mesh->faces;
     while ((c = fgetc(file)) != EOF) {
 	if (c == '#') {
 	    while (c != '\n' && c != EOF) c = fgetc(file);
 	} else if (c == 'v') {
-	    float x, y, z;
-	    fscanf(file, " %f %f %f", &x, &y, &z);
-	    float r, g, b, a;
-	    a = 1.0f;
-	    fgetc(file);
-	    /*
-	    if (fgetc(file) == ' ') {
-		fscanf(file, "%f %f %f %f\n", &r, &g, &b, &a);
+	    c = fgetc(file);
+	    if (c == 'n') {
+		float x, y, z, a;
+		fscanf(file, "%f %f %f\n", &x, &y, &z);
+		a = 1.0;
+		fgetc(file);
+		
+		mesh->vertices[normal*mesh->vertex_size+3] = x;
+		mesh->vertices[normal*mesh->vertex_size+4] = y;
+		mesh->vertices[normal*mesh->vertex_size+5] = z;
+		mesh->vertices[normal*mesh->vertex_size+6] = a;
+		
+		normal++;
+	    } else {
+		float x, y, z;
+		fscanf(file, "%f %f %f\n", &x, &y, &z);
+		fgetc(file);
+		mesh->vertices[vertex*mesh->vertex_size+0] = x;
+		mesh->vertices[vertex*mesh->vertex_size+1] = y;
+		mesh->vertices[vertex*mesh->vertex_size+2] = z;
+		vertex++;
 	    }
-	    */
-	    
-//	    printf("%f, %f, %f\n", x, y, z);
-	    
-	    *vertex++ = x;
-	    *vertex++ = y;
-	    *vertex++ = z;
-	    *vertex++ = r;
-	    *vertex++ = g;
-	    *vertex++ = b;
-	    *vertex++ = a;
 	} else if (c == 'f') {
 	    uint32_t x, y, z;
 	    fscanf(file, " %d %d %d\n", &x, &y, &z);
